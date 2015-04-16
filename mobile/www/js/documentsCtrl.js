0f6c184f0ac8c974c33062a7c41a564f8fd4cf94
fe-rod <federicorodriguezabo@gmail.com>
2015-04-16 01:36:49 -0300

    venue page with map, documents page skeleton

--- mobile/www/js/documentsCtrl.js
+++ mobile/www/js/documentsCtrl.js
@@ -1,5 +1,54 @@
 angular.module('lsm')
 
-    .controller('DocumentsCtrl', function($scope) {
+    .controller('DocumentsCtrl', function($scope, $cordovaInAppBrowser,$ionicPlatform) {
+        $scope.documents = [
+            {
+                name: 'Experiment Board Tutorial - EN',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo porttitor suscipit.' +
+                             ' Curabitur semper malesuada purus, non malesuada tellus facilisis ac',
+                isDoc: true
+            },
+            {
+                name: 'Experiment Board Tutorial - ES',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo porttitor suscipit.' +
+                    ' Curabitur semper malesuada purus, non malesuada tellus facilisis ac',
+                isDoc: true
+            },
+            {
+                name: 'LSM Intro',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo porttitor suscipit.' +
+                    ' Curabitur semper malesuada purus, non malesuada tellus facilisis ac',
+                isDoc: true
+            },
+            {
+                name: 'Extras',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo porttitor suscipit.' +
+                    ' Curabitur semper malesuada purus, non malesuada tellus facilisis ac',
+                isDoc: false
+            },
+            {
+                name: 'Material complementario',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo porttitor suscipit.' +
+                    ' Curabitur semper malesuada purus, non malesuada tellus facilisis ac',
+                isDoc: false
+            }
+        ];
 
+        $scope.openLink = function(doc){
+            var options = {
+                location: 'yes',
+                toolbar: 'no'
+            };
+
+            $ionicPlatform.ready(function() {
+                $cordovaInAppBrowser.open("http://www.kimhartman.se/wp-content/uploads/2013/10/the-lean-startup-summary.pdf", '_system', options)
+                    .then(function(event) {
+                        // success
+                    })
+                    .catch(function(event) {
+                        // error
+                    });
+            });
+
+        }
     });
--- mobile/www/js/venueCtrl.js
+++ mobile/www/js/venueCtrl.js
@@ -1,5 +1,45 @@
 angular.module('lsm')
 
-.controller('VenueCtrl', function($scope) {
+.controller('VenueCtrl', function($scope, $compile) {
+
+     function initialize() {
+        var myLatlng = new google.maps.LatLng(-34.878905,-56.079139);
+
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            streetViewControl: false,
+            zoomControl: false,
+            mapTypeControl: false
+        };
+        var map = new google.maps.Map(document.getElementById("map"),
+            mapOptions);
+
+        //Marker + infowindow + angularjs compiled ng-click
+        var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+        var compiled = $compile(contentString)($scope);
+
+        var infowindow = new google.maps.InfoWindow({
+            content: compiled[0]
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            title: 'Ingenio, Los Naranjos'
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map,marker);
+        });
+
+        $scope.map = map;
+    }
+
+    initialize()
+
+    //google.maps.event.addDomListener(window, 'load', initialize);
+
 
 });
